cff747b2ca0594c68c9ca8a4e1afb72b8e10e724
PedroACaldeira <caldeira.a.pedro@gmail.com>
2020-04-18 12:23:07 +0100

    Network visualization almost done

--- gspn_visualization.py
+++ gspn_visualization.py
@@ -15,10 +15,10 @@ def about():
 
 if __name__ == "__main__":  # on running python app.py
     my_pn = pn.GSPN()
-    places = my_pn.add_places(["p1", "p2", "p3"], [3, 0, 0])
-    trans = my_pn.add_transitions(["t1"], ["exp"], [1])
-    arc_in = {"p1": ["t1"]}
-    arc_out = {"t1": ["p2", "p3"]}
+    places = my_pn.add_places(['p1', 'p2', 'p3', 'p4'], [3, 0, 0, 0])
+    trans = my_pn.add_transitions(['t1'], ['exp'], [1])
+    arc_in = {'p1': ['t1'], 'p2': ['t1']}
+    arc_out = {'t1': ['p3', 'p4']}
     a, b = my_pn.add_arcs(arc_in, arc_out)
 
     app.run(debug=True)
