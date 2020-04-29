53775a4c0bae2e771c75597dc87b5c1aef83b4b2
PedroACaldeira <caldeira.a.pedro@gmail.com>
2020-04-29 12:31:16 +0100

    [GSPN-VIS] user can send xml through explorer

--- gspn_visualization.py
+++ gspn_visualization.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, jsonify, request
+import os
+
+from flask import Flask, render_template, jsonify, request, flash
 import gspn as pn
 import gspn_tools
+from werkzeug.utils import secure_filename, redirect
 
 app = Flask(__name__)  # create an app instance
 my_pn = pn.GSPN()
+
+ALLOWED_EXTENSIONS = set(['xml'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/")
 def home():
@@ -14,10 +24,10 @@ def home():
 def use_code():
     # Write your code here
     my_pn.add_places(['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12'],
-                              [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1])
+                     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1])
     my_pn.add_transitions(['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10'],
-                                  ['exp', 'exp', 'exp', 'exp', 'exp', 'imm', 'imm', 'exp', 'exp', 'exp'],
-                                  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+                          ['exp', 'exp', 'exp', 'exp', 'exp', 'imm', 'imm', 'exp', 'exp', 'exp'],
+                          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
     arc_in = {'p1': ['t1'], 'p2': ['t2'], 'p3': ['t3'], 'p5': ['t4'], 'p6': ['t4'], 'p7': ['t5'],
               'p8': ['t6', 't7'], 'p9': ['t8', 't9'], 'p10': ['t10'], 'p11': ['t4'], 'p12': ['t5']}
@@ -29,10 +39,25 @@ def use_code():
     return render_template("gspn_visualization_home.html", data=my_pn)
 
 
-@app.route("/use_xml")
+@app.route("/use_xml", methods=['POST'])
 def use_xml():
+    filename = ""
+    # Get file from user's explorer
+    # I saved the source for this code in the folder TESE
+    if request.method == 'POST':
+
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
+
     tool = gspn_tools.GSPNtools()
-    gspn = tool.import_xml("multi_escort_run.xml")[0]
+    gspn = tool.import_xml(filename)[0]
 
     # Begin First Part: add_places
     marking = gspn.get_current_marking()
