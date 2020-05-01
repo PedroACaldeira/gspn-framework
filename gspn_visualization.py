107b4868b1d268edb64086206a4dfe5b27fd54f6
PedroACaldeira <caldeira.a.pedro@gmail.com>
2020-05-01 14:49:49 +0100

    [GSPN-VIS] Highlight before transition firing complete.

--- gspn_visualization.py
+++ gspn_visualization.py
@@ -109,8 +109,6 @@ def use_xml():
 @app.route('/background_process_test')
 def background_process_test():
     marking_and_transition_list = my_pn.simulate()
-    print("lista", marking_and_transition_list)
-    print("marking", my_pn.get_current_marking())
     return jsonify(my_pn.get_current_marking(), marking_and_transition_list[1])
 
 
